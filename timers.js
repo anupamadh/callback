849542252a5260930dbdbfe29e4d202fc308117e
Anupama Dhir <sabhssabhs@yahoo.com>
2016-07-04 20:42:34 +0800

    First Commit

---
+++ timers.js
@@ -0,0 +1,37 @@
+// your code here:
+var secondsCounter = 0;
+var timerId;
+var setIntervalID;
+
+var resetBtn = document.getElementById('reset');
+var startBtn = document.getElementById('start');
+var pauseBtn = document.getElementById('pause');
+var buttons = document.querySelectorAll(".controls");
+var h1Element = document.getElementById('timer');
+
+startBtn.addEventListener("click", startWatch);
+resetBtn.addEventListener("click", resetWatch);
+pauseBtn.addEventListener("click", pauseWatch);
+
+function startWatch() {
+  h1Element.innerHTML  = secondsCounter;
+  setIntervalID=setInterval(updateTime, 1000);
+}
+
+function pauseWatch() {
+  clearInterval(setIntervalID);
+}
+
+ function updateTime (){
+   if (secondsCounter === 60) {
+     secondsCounter = 0;
+   }
+  secondsCounter += 1;
+  h1Element.innerHTML = secondsCounter;
+}
+
+function resetWatch() {
+  clearInterval(setIntervalID);
+  h1Element.innerHTML = "Stop Watch"
+  secondsCounter = 0
+}
